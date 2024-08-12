b1f13d395ce087ed975e0ae6a494778998fe4645
Akshansh Gupta <akshansh@Akshanshs-MacBook-Air.local>
2024-08-12 19:01:14 +0530

    backend core changes

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 const corsOptions = {
-   origin: true,
+   origin: 'https://travbook-frontend.vercel.app',
    credentials: true
 }
 
